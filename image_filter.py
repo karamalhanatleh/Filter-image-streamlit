937ee4bed607b404455771942bf561ebba4567c6
Karam ALhanatleh <93527002+karamalhanatleh@users.noreply.github.com>
2023-09-22 12:30:57 +0300

    Add files via upload

--- image_filter.py
+++ image_filter.py
@@ -8,8 +8,6 @@ Created on Fri Sep 22 12:18:58 2023
 import streamlit as st
 from PIL import Image
 import numpy as np
-
-# Capture an image from the camera
 pic = st.camera_input("Take a pic")
 
 if pic:
@@ -22,8 +20,19 @@ if pic:
     
     # Display the processed image without a white border
     st.image(inverted_img, use_column_width=True)
-
+    if st.button("Save Image"):
+        # Save the displayed image using PIL
+        save_path = "image_255.png"
+        save_img = Image.fromarray(inverted_img)
+        save_img.save(save_path)
+        st.success(f"Image saved as {save_path}")
+    
+    
+    
     st.image(150 - arr_img, use_column_width=True)
-
-
-
+    if st.button("Save Image2"):
+        # Save the displayed image using PIL
+        save_path = "image_150.png"
+        save_img = Image.fromarray(inverted_img)
+        save_img.save(save_path)
+        st.success(f"Image saved as {save_path}")
